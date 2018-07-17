825af7cd8fcda1c1f1aba987b4623433a402c304
cd-xulei <chengduxulei@foxmail.com>
2018-07-17 20:54:58 +0800

    心跳

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const logger = require('./helpers/logger.js').getLogger('index')
 const PORT = 3389
 const server = net.createServer()
 
+const heart = require('./protocol/heart.js')
+
 server.on('connection', function (socket) {
   // 从连接中读取数据
   socket.on('data', function (data) {
     logger.debug('原始数据', data)
     logger.debug('接收的数据 hex:', data.toString('hex'))
+    heart(data)
   })
   // 删除被关闭的连接
   socket.on('close', function () {
---
+++ protocol/heart.js
@@ -0,0 +1,70 @@
+'use strict'
+
+/**
+ * 解析心跳包
+ */
+
+const logger = require('../helpers/logger.js').getLogger('heart')
+const crypto = require('crypto')
+
+const pwdBuffer = Buffer.from([
+  0x42, 0x12, 0xee, 0x75, 0xea, 0x88, 0x58, 0x66, 0x79, 0xa8,
+  0x6a, 0x48, 0xae, 0xc8, 0xbb, 0xd7, 0xd4, 0x12, 0x01, 0xae,
+  0xc6, 0x00, 0xce, 0x88, 0xaa, 0xae, 0x64, 0xaa, 0x00, 0x00,
+  0x00, 0x00
+])
+
+const fixVal = Buffer.from([
+  0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0xee, 0x00, 0x00, 0x00, 0x00,
+  0x01, 0x23, 0x00, 0x00
+])
+
+let MachineType
+(function (MachineType) {
+  MachineType[MachineType['MachineTypeNormal'] = 1] = 'MachineTypeNormal'
+  MachineType[MachineType['MachineTypeDirect'] = 2] = 'MachineTypeDirect'
+  // MachineType[MachineType["MachineTypeTurnover"] = 3] = "MachineTypeTurnover";
+})(MachineType || (MachineType = {}))
+
+function buildHeartReply (rawBuffer) {
+  let beginSign = rawBuffer.readUInt16BE(0)// 头
+  let machineId = rawBuffer.toString('ascii', 2, 18)// 设备id
+  console.log(machineId)
+  let random = rawBuffer.readUInt32LE(18)// 随机值
+  console.log(random)
+  let version = rawBuffer.toString('hex', 22, 26)// 固定版本
+
+  let Speed = rawBuffer.readUInt16BE(26)// 风机转速
+  let wifi = rawBuffer.readUInt8(28)// wifi强度
+  let mobileData = rawBuffer.readUInt8(29)// 移动数据强度
+  let flag = rawBuffer.readUInt8(30)// 发送标志 0-wifi 1-移动数据
+  let frameType = rawBuffer.readUInt8(31)// 帧类型 0-心跳 1-命令
+  logger.debug('帧类型', frameType)
+
+  if (frameType === 1) return
+
+  const nowTime = ~~(Date.now() / 1000)
+  const mType = MachineType.MachineTypeNormal
+  const resBuffer = Buffer.from(64)
+  resBuffer.writeUInt16LE(0x55AA, 0) // 头
+  resBuffer.write(machineId, 2, 16, 'ascii')// 机器id
+  resBuffer.writeUInt32LE(random, 18)// 随机码
+  resBuffer.writeUInt32LE(nowTime, 22)// 当前时间
+  if (mType === MachineType.MachineTypeDirect) {
+    resBuffer.writeUInt32LE(nowTime + 300, 26)
+  } else {
+    resBuffer.writeUInt32LE(activitedTime, 26)
+  }// 关闭时间
+  resBuffer.writeInt8(0x00, 30)// 保留位
+  resBuffer.writeInt8(0x00, 31)// 帧类型
+
+  let clcData = Buffer.alloc(88)
+  pwdBuffer.copy(clcData, 0)
+  resBuffer.copy(clcData, 32, 0, 32)
+  fixVal.copy(clcData, 64)
+  let HashFunc = crypto.createHash('sha256')
+  HashFunc.update(clcData)
+  HashFunc.digest().copy(resBuffer, 32)
+  logger.debug('心跳报文', resBuffer)
+}
