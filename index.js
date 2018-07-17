efd32cfb17f028fd03a3330c7af7ada700079175
cd-xulei <chengduxulei@foxmail.com>
2018-07-17 14:38:38 +0800

    init

---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const net = require('net')
+const logger = require('./helpers/logger.js').getLogger('index')
+
+const PORT = 3389
+const server = net.createServer()
+
+server.on('connection', function (socket) {
+  // 从连接中读取数据
+  socket.on('data', function (data) {
+    logger.debug('原始数据', data)
+    logger.debug('接收的数据 hex:', data.toString('hex'))
+  })
+  // 删除被关闭的连接
+  socket.on('close', function () {
+  })
+})
+
+server.on('error', function (err) {
+  logger.error(err.message)
+})
+
+server.on('close', function () {
+  logger.debug('server close')
+})
+
+server.listen(PORT, () => {
+  logger.debug(`tcp server listening on ${PORT}`)
+  setInterval(() => {
+    server.getConnections((err, count) => {
+      if (err) {
+        return logger.error(err.message)
+      }
+      if (count > 0) {
+        logger.debug('连接数量', count)
+      }
+    })
+  }, 2000)
+})
