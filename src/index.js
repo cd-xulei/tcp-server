bb3c5fbc196435e013157108271ef0ef704157c8
cd-xulei <chengduxulei@foxmail.com>
2018-07-19 11:58:04 +0800

    捕获异常错误

--- src/index.js
+++ src/index.js
@@ -13,38 +13,47 @@ const heart = require('./protocol/heart.js')
 const sockets = {}
 
 server.on('connection', function (socket) {
+  server.getConnections((err, count) => {
+    if (err) {
+      return logger.error('读取连接数量出错:', err.message)
+    }
+    if (count) {
+      logger.debug('连接数量:', count)
+    }
+  })
   // 从连接中读取数据
   socket.on('data', async (buffer) => {
-    logger.debug('接收的数据 hex:', buffer.toString('hex'))
+    logger.debug('接收的hex串:', buffer.toString('hex'))
     if (!Buffer.isBuffer(buffer)) return
     if (buffer.length < 32) return
     if (buffer.toString('hex', 0, 2) !== 'aa55') return
     const res = await heart(buffer)
     // 经历过心跳后 每个 socket都应该被存储
-    sockets[res.machineId] = Object.assign(_.omit(res, ['resBuffer']), { socket })
-    sockets[res.machineId].socket.write(res.resBuffer)
+    // sockets[res.machineId] = Object.assign(_.omit(res, ['resBuffer']), { socket })
+    socket.write(res.resBuffer)
   })
   // 删除被关闭的连接
-  socket.on('close', function () {
+  socket.on('close', function (bool) {
+    const log = bool ? '传输错误引起 socket 关闭' : 'socket 关闭连接'
+    logger.debug(log)
+  })
+  socket.on('error', function (err) {
+    logger.error('socket 报错:', err.message)
+    logger.error('socket 报错:', err.stack)
+    socket.destroy()
   })
 })
 
 server.on('error', function (err) {
-  logger.error(err.message)
+  logger.error('tcp服务报错:', err.message)
+  logger.error('tcp服务报错:', err.stack)
 })
 
 server.on('close', function () {
-  logger.debug('server close')
+  logger.debug('tcp服务关闭')
 })
 
 server.listen(PORT, () => {
   const address = server.address()
   logger.debug(`tcp server listening on ${address.port}`)
-  setInterval(() => {
-    server.getConnections((err, count) => {
-      if (err) {
-        return logger.error(err.message)
-      }
-    })
-  }, 2000)
 })
