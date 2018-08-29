37027a0af56948731459ff7c17410a820497ddf9
cd-xulei <chengduxulei@foxmail.com>
2018-08-29 11:10:20 +0800

    feat: 添加 eslint

--- src/protocol/heart.js
+++ src/protocol/heart.js
@@ -16,103 +16,103 @@ MachineType[MachineType['MachineTypeDirect'] = 2] = 'MachineTypeDirect'
 MachineType[MachineType['MachineTypeTurnover'] = 3] = 'MachineTypeTurnover'
 
 module.exports = async function heartHandler (params) {
-  const { beginSign, machineId, random, version, speed, wifi, mobileData, flag, frameType } = params
+    const { beginSign, machineId, random, version, speed, wifi, mobileData, flag, frameType } = params
 
-  logger.debug('当前数据包类型: 心跳')
+    logger.debug('当前数据包类型: 心跳')
 
-  let mType = MachineType.MachineTypeNormal
-  logger.debug(`设备id: ${machineId}`)
+    let mType = MachineType.MachineTypeNormal
+    logger.debug(`设备id: ${machineId}`)
 
-  const stateStr = await redisCli.get(`${machineId}_state`)
-  if (stateStr) {
-    let mState
-    try {
-      mState = JSON.parse(stateStr)
-    } catch (e) {
-      mState = null
+    const stateStr = await redisCli.get(`${machineId}_state`)
+    if (stateStr) {
+        let mState
+        try {
+            mState = JSON.parse(stateStr)
+        } catch (e) {
+            mState = null
+        }
+        if (!_.isEmpty(mState)) {
+            if (mState.mode === 2 || mState.mode === 3) {
+                mType = MachineType.MachineTypeDirect
+            }
+        }
+        if (flag === 0) {
+            mState.wifi = wifi
+            mState.mobile = mobileData
+        }
+        if (flag === 1) {
+            mState.wifi = wifi
+            mState.mobile = mobileData
+        }
+        mState.flag = flag
+        mState.speed = speed
+        mState.version = buildVersionStr(version)
+        await redisCli.set(`${machineId}_state`, JSON.stringify(mState))
     }
-    if (!_.isEmpty(mState)) {
-      if (mState.mode === 2 || mState.mode === 3) {
-        mType = MachineType.MachineTypeDirect
-      }
-    }
+    const logString = `模式: ${mType}, 版本: ${buildVersionStr(version)}, 转速: ${speed}`
     if (flag === 0) {
-      mState.wifi = wifi
-      mState.mobile = mobileData
+        logger.debug(`信号方式: wifi; 信号强度: ${wifi}, ${logString}`)
     }
     if (flag === 1) {
-      mState.wifi = wifi
-      mState.mobile = mobileData
+        logger.debug(`信号方式: gprs; 信号强度: ${mobileData}, ${logString}`)
     }
-    mState.flag = flag
-    mState.speed = speed
-    mState.version = buildVersionStr(version)
-    await redisCli.set(`${machineId}_state`, JSON.stringify(mState))
-  }
-  const logString = `模式: ${mType}, 版本: ${buildVersionStr(version)}, 转速: ${speed}`
-  if (flag === 0) {
-    logger.debug(`信号方式: wifi; 信号强度: ${wifi}, ${logString}`)
-  }
-  if (flag === 1) {
-    logger.debug(`信号方式: gprs; 信号强度: ${mobileData}, ${logString}`)
-  }
 
-  const nowTime = ~~(Date.now() / 1000)
+    const nowTime = ~~(Date.now() / 1000)
 
-  let resBuffer = Buffer.alloc(32)
-  resBuffer.writeUInt16LE(0x55AA, 0) // 头
-  resBuffer.write(machineId, 2, 16, 'ascii')// 机器id
-  resBuffer.writeUInt32LE(random, 18)// 随机码
-  logger.debug('当前时间:', nowTime)
-  resBuffer.writeUInt32LE(nowTime, 22)// 当前时间
+    let resBuffer = Buffer.alloc(32)
+    resBuffer.writeUInt16LE(0x55AA, 0) // 头
+    resBuffer.write(machineId, 2, 16, 'ascii')// 机器id
+    resBuffer.writeUInt32LE(random, 18)// 随机码
+    logger.debug('当前时间:', nowTime)
+    resBuffer.writeUInt32LE(nowTime, 22)// 当前时间
 
-  let activitedTimeStr = await redisCli.get(`${machineId}_activited`)
-  let activitedTime = !activitedTimeStr ? 0 : Number(activitedTimeStr)
+    let activitedTimeStr = await redisCli.get(`${machineId}_activited`)
+    let activitedTime = !activitedTimeStr ? 0 : Number(activitedTimeStr)
 
-  let LastTimeStr = await redisCli.get(`${machineId}_lastTick`)
-  let LastTime = !LastTimeStr ? 0 : Number(LastTimeStr)
-  await redisCli.set(`${machineId}_lastTick`, nowTime.toString())
+    let LastTimeStr = await redisCli.get(`${machineId}_lastTick`)
+    let LastTime = !LastTimeStr ? 0 : Number(LastTimeStr)
+    await redisCli.set(`${machineId}_lastTick`, nowTime.toString())
 
-  // 判断当时设备是否在使用
-  if (speed > 1) {
+    // 判断当时设备是否在使用
+    if (speed > 1) {
     // 判定上次是否在使用中
-    if (await redisCli.exists(`${machineId}_lastUse`)) {
-      let useTime = nowTime - LastTime
-      if (useTime <= 300) {
-        await redisCli.incrby(`${machineId}_useTime`, useTime)
-      }
+        if (await redisCli.exists(`${machineId}_lastUse`)) {
+            let useTime = nowTime - LastTime
+            if (useTime <= 300) {
+                await redisCli.incrby(`${machineId}_useTime`, useTime)
+            }
+        }
+        await redisCli.set(`${machineId}_lastUse`, 1)
+        await redisCli.expire(`${machineId}_lastUse`, 300)
+    } else {
+        await redisCli.del(`${machineId}_lastUse`)
     }
-    await redisCli.set(`${machineId}_lastUse`, 1)
-    await redisCli.expire(`${machineId}_lastUse`, 300)
-  } else {
-    await redisCli.del(`${machineId}_lastUse`)
-  }
 
-  let closeTime = mType === MachineType.MachineTypeDirect ? nowTime + 300 : activitedTime
-  logger.debug('关闭时间:', closeTime)
-  resBuffer.writeUInt32LE(closeTime, 26)
+    let closeTime = mType === MachineType.MachineTypeDirect ? nowTime + 300 : activitedTime
+    logger.debug('关闭时间:', closeTime)
+    resBuffer.writeUInt32LE(closeTime, 26)
 
-  resBuffer.writeInt8(0x00, 30)// 保留位
-  resBuffer.writeInt8(0x00, 31)// 帧类型
+    resBuffer.writeInt8(0x00, 30)// 保留位
+    resBuffer.writeInt8(0x00, 31)// 帧类型
 
-  // 加上校验
-  resBuffer = hashFunc(resBuffer)
-  logger.debug('回复心跳 hex串:', resBuffer.toString('hex'))
-  logger.debug('\r\n')
-  return {
-    resBuffer,
-    machineId,
-    frameType
-  }
+    // 加上校验
+    resBuffer = hashFunc(resBuffer)
+    logger.debug('回复心跳 hex串:', resBuffer.toString('hex'))
+    logger.debug('\r\n')
+    return {
+        resBuffer,
+        machineId,
+        frameType
+    }
 }
 
 function buildVersionStr (version) {
-  let versionStr = 'V'
-  for (let i = 0; i < 4; i++) {
-    versionStr += parseInt(version.substr(i * 2, 2))
-    if (i !== 3) {
-      versionStr += '.'
+    let versionStr = 'V'
+    for (let i = 0; i < 4; i++) {
+        versionStr += parseInt(version.substr(i * 2, 2))
+        if (i !== 3) {
+            versionStr += '.'
+        }
     }
-  }
-  return versionStr
+    return versionStr
 }
--- src/protocol/parse.js
+++ src/protocol/parse.js
@@ -3,17 +3,17 @@
 // const logger = require('../helpers/logger.js').getLogger('parse')
 
 module.exports = async function (rawBuffer) {
-  const params = {
-    beginSign: rawBuffer.toString('hex', 0, 2), // 头
-    machineId: rawBuffer.toString('ascii', 2, 18), // 设备id
-    random: rawBuffer.readUInt32LE(18), // 随机值
-    version: rawBuffer.toString('hex', 22, 26), // 固定版本
+    const params = {
+        beginSign: rawBuffer.toString('hex', 0, 2), // 头
+        machineId: rawBuffer.toString('ascii', 2, 18), // 设备id
+        random: rawBuffer.readUInt32LE(18), // 随机值
+        version: rawBuffer.toString('hex', 22, 26), // 固定版本
 
-    speed: rawBuffer.readUInt16LE(26), // 风机转速
-    wifi: rawBuffer.readUInt8(28), // wifi强度
-    mobileData: rawBuffer.readUInt8(29), // 移动数据强度
-    flag: rawBuffer.readUInt8(30), // 发送标志 0-wifi 1-移动数据
-    frameType: rawBuffer.readUInt8(31) // 帧类型 0-心跳 1-命令
-  }
-  return params
+        speed: rawBuffer.readUInt16LE(26), // 风机转速
+        wifi: rawBuffer.readUInt8(28), // wifi强度
+        mobileData: rawBuffer.readUInt8(29), // 移动数据强度
+        flag: rawBuffer.readUInt8(30), // 发送标志 0-wifi 1-移动数据
+        frameType: rawBuffer.readUInt8(31) // 帧类型 0-心跳 1-命令
+    }
+    return params
 }
